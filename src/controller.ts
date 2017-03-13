83f63ca99ef3c77c17e0c3678884345a8ceae2c6
Ben Rachbach <ben.rachbach@makewonder.com>
2017-03-13 14:01:10 -0700

    setup that I think will work

--- src/controller.ts
+++ src/controller.ts
@@ -1,3 +1,25 @@
-module.exports = () => (req, res) => {
-  res.status(200).send('yay');
-}
+/* eslint camelcase:0 */
+
+const marked = require('marked');
+const _ = require('lodash');
+const MCService = require('./MCService');
+
+const handleError = (req, res) => (error) => {
+  if (error.status === 400 && error.data.title === "Invalid Resource") {
+    res.status(400).json(error.data);
+  } else {
+    console.error("Got unexpected error from mailchimp subscribe request. Error was %o, and original config: %o", error, error.config);
+    res.status(500).end();
+  }
+};
+
+module.exports.subscribe = MAILCHIMP_API_KEY => {
+
+  const mcService = new MCService(MAILCHIMP_API_KEY);
+
+  return (req, res) => {
+    return mcService.subscribe(req.body)
+      .then(() => res.status(201).end())
+      .catch(handleError(req, res));
+  };
+};
---
+++ src/service.ts
@@ -0,0 +1,22 @@
+const axios = require('axios');
+crypto = require('crypto');
+
+const md5 = (str = "") => crypto.createHash("md5").update(str.toLowerCase()).digest("hex");
+
+class MCService {
+  constructor (MAILCHIMP_API_KEY) {
+    if (!MAILCHIMP_API_KEY) {
+      throw new Error('must provide Mailchimp API key')
+    }
+
+    this.MAILCHIMP_API_KEY = MAILCHIMP_API_KEY;
+    this.MAILCHIMP_DATACENTER = this.MAILCHIMP_API_KEY.split('-')[1];
+    this.MAILCHIMP_ENDPOINT = `https://playi:${ this.MAILCHIMP_API_KEY }@${ this.MAILCHIMP_DATACENTER }.api.mailchimp.com/3.0`;
+  }
+  subscribe (body) {
+    const hash = md5(body.email_address);
+    return axios.put(`${ MAILCHIMP_ENDPOINT }/lists/${ config.MAILCHIMP_TEACHER_MAILING_LIST }/members/${ hash }`, body);
+  } 
+}
+
+module.exports = MCService;
