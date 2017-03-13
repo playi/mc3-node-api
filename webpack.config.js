9ed27c48ca5f703dffd524c3d85d4d099c3066de
Ben Rachbach <ben.rachbach@makewonder.com>
2017-03-13 12:37:21 -0700

    More setup and shrinkwrap

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+var argv = require('yargs').argv;
+argv.env = argv.env || "browser";
+var env = argv.env === "browser" ? "browser" : "device";
+var autoprefixer = require('autoprefixer');
+global.Promise = require('bluebird');
+
+var devPlugins = [
+  new webpack.DefinePlugin({
+    "process.env.NODE_ENV": JSON.stringify("development")
+  })
+];
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    "process.env.NODE_ENV": JSON.stringify("production")
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    beautify: true
+  })
+];
+
+//noinspection JSUnresolvedFunction
+module.exports = {
+  entry: './src',
+  devtool: !argv.production && "inline-source-map",
+  resolve: {
+    extensions: ["", ".webpack.js", ".web.js", ".js", ".ts", ".tsx"]
+  },
+  plugins: argv.production ? prodPlugins : devPlugins,
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ["ts-loader"]
+      }
+    ]
+  },
+  postcss: function(){
+    return [autoprefixer];
+  },
+  output: {
+    path: "/dist",
+    publicPath: "",
+    filename: "[name].js",
+    library: "[name]",
+    libraryTarget: "umd"
+  }
+};
